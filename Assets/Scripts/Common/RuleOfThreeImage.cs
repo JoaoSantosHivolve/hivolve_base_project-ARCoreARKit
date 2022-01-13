6c9dc72e3e0c378db001ea40daf42dc6c4c41bdf
Lucas Santos <joao.santos@hivolve.com>
2022-01-13 11:07:28 +0000

    Project reorder

---
+++ Assets/Scripts/Common/RuleOfThreeImage.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Common
+{
+    public enum SideToAdjust
+    {
+        Width,
+        Height
+    }
+
+    public class RuleOfThreeImage : MonoBehaviour
+    {
+        public float originalWidth;
+        public float originalHeight;
+
+        public SideToAdjust sideToAdjust;
+
+        private void Awake()
+        {
+            Canvas.ForceUpdateCanvases();
+
+            var t = GetComponent<RectTransform>();
+            float newLength = 0;
+            switch (sideToAdjust)
+            {
+                case SideToAdjust.Width:
+                    newLength = (t.rect.height * originalWidth) / originalHeight;
+                    t.sizeDelta = new Vector2(newLength, 0);
+                    break;
+                case SideToAdjust.Height:
+                    newLength = (t.rect.width * originalHeight) / originalWidth;
+                    t.sizeDelta = new Vector2(0, newLength);
+                    break;
+            }
+        }
+    }
+}
